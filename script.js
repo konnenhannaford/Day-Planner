7a6fc162d6bac6641e2bd8fa8209fd3e8f7c2349
konnenhannaford <konnen23@gmail.com>
2021-09-23 19:06:29 +1000

    updated to start adding in rows for planner

--- script.js
+++ script.js
@@ -5,21 +5,36 @@ timeblock()
 })
 
 function timeblock (){
-var times = {"9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"}
+var times = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"]
+for (var i=0; i<times.length; i++) {
 
-for (vari=1; i<times.length; i++) {
+
     var nexttime = $("#example").clone();
-    nexttime.atr("id", times[i]);
+    nexttime.attr("id", times[i]);
     nexttime.children(".hour").text(times[i]);
     nexttime.appendto(".container");
+    
+    var timeblocktextbox = $("#${times[i]}")
+    
+    createTimeBasedClasses(timesArr,i,timeBlockTextarea)
 
-    var timeblocktextbox = $('#${times[i]}')
+    timeBlockTextarea.children(".saveBtn").on('click',saveToLocalStorage);
 
-    // past 
-    if (moment().format ("ha") === times[i]) timebloxktextbox.children(".description").addclass("present");
-    // present
-    if (moment().format ("ha") === times[i]) timebloxktextbox.children(".description").addclass("present");
-    // future
-    if (moment().format ("ha") === times[i]) timebloxktextbox.children(".description").addclass("present");
+}
+    $("#sample").remove()
+    showLocalStorageValues()
 }
 
+
+function createTimeBasedClasses(timesArr,i,timeBlockTextarea) 
+{
+
+    // past 
+    if (moment().format ("hA") === times[i]) timeblocktextbox.children(".description").addclass("present");
+    // present
+    else if (moment( times[i], "hA").isBefore ()) timeblocktextbox.children(".description").addclass("past");
+    // future
+    else timeblocktextbox.children(".description").addClass("future");
+      }
+    
+    
