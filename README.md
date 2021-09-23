b4c53c09008917fd13be80f1d700a2e58ec1fec0
konnenhannaford <konnen23@gmail.com>
2021-09-23 23:57:17 +1000

    updated readme to cover all info

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Day-Planner
+
+Site can be accessed here: https://konnenhannaford.github.io/Day-Planner/
+The source can be retrieved by cloning and pulling from https://github.com/konnenhannaford/Day-Planner
+
+# About
+This page is a Daily Planner that allows the user to input information to schedule an upcoming day
+
+# Tools used
+HTML, CSS, Javascript, J-Query
+
+# Updates
+* Site has been developed to include an hourly schedule for an upcoming day
+* Site has been developed to allow users to input information into daily slots
+* Site has been developed to allow information to be saved if the page is refreshed
+* Site has been developed to inlcude a moment tag to show currrent date and time
+* Site has been developed to coour time slots by determining which fall into the past, present and future  
+* Coding for site has been streamlined for efficiency
+* A repo following best practice has been created
+* A ReadMe detailing the site and its workings has been created
+* Site has been deployed as a live site and applications loads with no errors
+
+# Making Changes
+Repo is public and can be updated by all via https://github.com/konnenhannaford/Java-Quizz
+
+# Learnings and changes
+Completing this has increased familiarity with the process of J-Query and its applications to a website.  It is very complex area that will require further investigation as it is essential to a large are of roles and tasks.  
+
+# Site Screenshot
+![Daily-Planner](screencapture-file-Users-konnen-bootcamp-homework-fourth-Java-Quizz-index-html-2021-09-18-15_10_14.png)
+
--- script.js
+++ script.js
@@ -1,59 +1,59 @@
 $(document).ready(function(){
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-    timeblock()
+    createTimeRows()
 })
 
-function timeblock (){
-    var times = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"]
-    for (var i=0; i < times.length; i++) {
 
-    var nexttime = $("#example").clone();
-    nexttime.attr("id", times[i]);
-    nexttime.children(".hour").text(times[i]);
-    nexttime.appendTo(".container");
-    
-    var timeblocktextbox = $(`#${times[i]}`)
-    
-    createTimeBasedClasses(timesArr,i,timeblocktextbox)
+function createTimeRows() {
+    let timesArr= ["9AM","10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]; 
+    for (let i=0; i < timesArr.length; i++){
 
-    timeblocktextbox.children(".saveBtn").on('click',saveToLocalStorage);
+    let newTimeBlockEL= $("#sample").clone();
+    
+    newTimeBlockEL.attr("id", timesArr[i]); 
+
+    newTimeBlockEL.children(".hour").text(timesArr[i]);
+    newTimeBlockEL.appendTo(".container"); 
+    
+    let timeBlockTextarea = $(`#${timesArr[i]}`)
+
+    createTimeBasedClasses(timesArr,i,timeBlockTextarea)
+
+    timeBlockTextarea.children(".saveBtn").on('click',saveToLocalStorage);
 
 }
-    $("#example").remove()
+    $("#sample").remove()
     showLocalStorageValues()
+}
 
 
-
-function createTimeBasedClasses(times,i,timeblocktextbox) {
-
-    // past 
-    if (moment().format ("hA") === times[i]) timeblocktextbox.children(".description").addclass("present");
-    // present
-    else if (moment( times[i], "hA").isBefore ()) timeblocktextbox.children(".description").addclass("past");
-    // future
-    else timeblocktextbox.children(".description").addClass("future");
-      }
-      
-let storageArray = []
+function createTimeBasedClasses(timesArr,i,timeBlockTextarea) {
     
+    if (moment().format("hA") === timesArr[i]) timeBlockTextarea.children(".description").addClass("present");
+    else if (moment(timesArr[i], "hA").isBefore())  timeBlockTextarea.children(".description").addClass("past");
+    else timeBlockTextarea.children(".description").addClass("future"); 
+}
+
+let storageArray = []
+
 function saveToLocalStorage() {
 
-        let textAreaValue = $(this).siblings("textarea").val();
-        let time= $(this).siblings("p").text();
-        const storageObj = {time,textAreaValue}
-        storageArray.push(storageObj)
-        localStorage.setItem('storageArray', JSON.stringify(storageArray));
-        localStorage.getItem("storageArray")
+    let textAreaValue = $(this).siblings("textarea").val();
+    let time= $(this).siblings("p").text();
+    const storageObj = {time,textAreaValue}
+    storageArray.push(storageObj)
+    // console.log(storageArray)
+    localStorage.setItem('storageArray', JSON.stringify(storageArray));
+    // localStorage.getItem("storageArray")
+}
+
+
+function showLocalStorageValues() {
+    let savedData = JSON.parse(localStorage.getItem("storageArray"))
+    if (savedData){
+        savedData.forEach(element => {
+            let targetTextArea = $(`#${element.time}`)
+            targetTextArea.children("textarea").val(element.textAreaValue)
+        });
     }
-    
-    
-    function showLocalStorageValues() {
-        var savedData = JSON.parse(localStorage.getItem("storageArray"))
-        if (savedData){
-            savedData.forEach(element => {
-                var targetTextArea = $(`#${element.time}`)
-                targetTextArea.children("textarea").val(element.textAreaValue)
-            });
-        }
-    }
-    
+}
